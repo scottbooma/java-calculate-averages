891fde0e2eb1235ecd67a44fb44bba03971756e6
scottbooma <scottbooma@gmail.com>
2022-05-24 17:02:45 -0400

    Add solution

--- App.java
+++ App.java
@@ -1,4 +1,36 @@
+import java.util.Arrays;
+
 class App {
   public static void main(String args[]){
+
+  System.out.println(String.format("The lowest score is %d", App.lowestScore(scores)));
+  System.out.println(String.format("The highest score is %d", App.highestScore(scores)));
+  System.out.println(String.format("The mean score is %f", App.meanScore(scores)));
+  System.out.println(String.format("The median score is %f", App.medianScore(scores)));
   }
-}
+
+  private static int[] scores = {72, 95, 91, 85, 87, 51, 65, 72, 75, 89};
+  
+  private static int lowestScore(int[] scoreArray){
+    Arrays.sort(scoreArray);
+    return scoreArray[0];
+  };
+
+  private static int highestScore(int[] scoreArray){
+    Arrays.sort(scoreArray);
+    return scoreArray[scoreArray.length - 1];
+  }
+
+  private static double meanScore(int[] scoreArray){
+    double sum = 0;
+    for (int i = 0; i < scoreArray.length; i++){
+      sum = sum + scoreArray[i];
+    };
+    return sum / scoreArray.length;
+  }
+
+  private static double medianScore(int[] scoreArray){
+    return (scoreArray[scoreArray.length / 2] + scoreArray[(scoreArray.length / 2) - 1]) / 2;
+  }
+};
+
